627d8d80dc8fae2ea2eda8d8150f4989bcbb2f80
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-03-22 01:00:29 -0400

    create private route

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import {
 } from 'react-router-dom';
 
 import { BooksProvider } from './BooksContext';
+import { PrivateRoute } from './components/PrivateRoute';
 import Books from './components/Books';
 import Book from './components/Book';
 import NotFound from './components/NotFound';
+import SecretBooks from './components/SecretBooks';
 import './App.css';
 
 function App() {
@@ -20,6 +22,10 @@ function App() {
           <Route path="/books" element={<Navigate to="/" />} />
           <Route path="/books/:bookID" element={<Book />} />
           <Route path="*" element={<NotFound />} />
+          <Route
+            path="/secret"
+            element={<PrivateRoute Component={<SecretBooks />} />}
+          />
         </Routes>
       </Router>
     </BooksProvider>
